65b9eba8bd690f40e95887b58e94c19f347ebc37
Andrey <ivanov@ukrtatnafta.com>
2016-10-04 10:56:03 +0300

    2016-10-03

---
+++ src/main/java/ua/inventory/server/Server.java
@@ -0,0 +1,58 @@
+package ua.inventory.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by ivanov-av
+ * 04.10.2016 10:34.
+ */
+public class Server {
+    private ServerSocket server;
+    public Integer server_port; // tcp port to listen on
+    public static void main(String[] args) throws IOException {
+        System.out.println("Welcome to Server side");
+        BufferedReader in = null;
+        PrintWriter out= null;
+
+        ServerSocket servers = null;
+        Socket fromclient = null;
+
+        // create server socket
+        try {
+            servers = new ServerSocket(4444);
+        } catch (IOException e) {
+            System.out.println("Couldn't listen to port 4444");
+            System.exit(-1);
+        }
+
+        try {
+            System.out.print("Waiting for a client...");
+            fromclient= servers.accept();
+            System.out.println("Client connected");
+        } catch (IOException e) {
+            System.out.println("Can't accept");
+            System.exit(-1);
+        }
+
+        in  = new BufferedReader(new
+                InputStreamReader(fromclient.getInputStream()));
+        out = new PrintWriter(fromclient.getOutputStream(),true);
+        String         input,output;
+
+        System.out.println("Wait for messages");
+        while ((input = in.readLine()) != null) {
+            if (input.equalsIgnoreCase("exit")) break;
+            out.println("S ::: "+input);
+            System.out.println(input);
+        }
+        out.close();
+        in.close();
+        fromclient.close();
+        servers.close();
+    }
+}
