c6c7161961854196c949a60ca447d009db4c3d72
Andrey <ivanov@ukrtatnafta.com>
2016-10-04 10:23:56 +0300

    2016-10-03

---
+++ src/main/java/ua/inventory/agent/Main.java
@@ -0,0 +1,11 @@
+package ua.inventory.agent;
+
+/**
+ * Created by ivanov-av
+ * 04.10.2016 10:11.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
